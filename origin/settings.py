6ab53e44ff41ead3fbdd27587f261f21317be63b
Jat_M <velizj4scsu@gmail.com>
2023-03-17 11:39:15 -0400

    Debug changed to false

--- origin/settings.py
+++ origin/settings.py
@@ -10,7 +10,7 @@ from google.cloud import secretmanager
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-env = environ.Env(DEBUG=(bool, True))
+env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
 
 try:
